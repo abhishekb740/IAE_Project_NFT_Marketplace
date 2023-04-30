6dbf56b9517c4da01f1493452650146cfb1f0e4a
abhishekb740 <abhishekb740@gmail.com>
2023-04-30 13:12:54 +0530

    Marketplace.json file updated.

--- src/components/MarketPlace.jsx
+++ src/components/MarketPlace.jsx
@@ -69,7 +69,7 @@ export default function Marketplace() {
             <Navbar></Navbar>
             <div className="flex flex-col place-items-center mt-20">
                 <div className="md:text-xl font-bold text-white">
-                    Top NFTs
+                    Top Antique Items (NFTs)
                 </div>
                 <div className="flex mt-5 justify-between flex-wrap max-w-screen-xl text-center">
                     {data.map((value, index) => {
